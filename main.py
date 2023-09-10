98b498f55fcd67d704f38bd306df9db8c569f88a
Braulio Mercado <b_mercado@mail.fhsu.edu>
2023-09-10 19:48:34 -0400

    Collecting data for first stock

--- main.py
+++ main.py
@@ -25,3 +25,14 @@ headers = {
 response = requests.post(url, data=payload, headers=headers)
 
 pprint.pprint(response.json())
+
+data = response.json()
+
+adj_close_values = [item['Adj Close'] for item in data['data']]
+
+print(adj_close_values)
+
+#aapl [178.6100006104, 180.1900024414, 184.1199951172, 187.6499938965, 187.8699951172, 189.4600067139, 189.6999969482, 182.9100036621, 177.5599975586, 178.1799926758]
+aapl_close_price = [178.6100006104, 180.1900024414, 184.1199951172, 187.6499938965, 187.8699951172,
+189.4600067139, 189.6999969482, 182.9100036621, 177.5599975586, 178.1799926758]
+
